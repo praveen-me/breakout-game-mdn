5885c484a9fee3a1d0dd377549b8b148d24a7986
praveen-me <praveenkrsaini0@gmail.com>
2018-10-08 16:08:20 +0530

    Add bricks to ;the game

--- main.js
+++ main.js
@@ -10,6 +10,21 @@ const paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+let bricks = [];
+for(let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for(let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = {x : 0, y : 0};
+  }
+}
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
@@ -46,8 +61,25 @@ const drawPaddle = () => {
   ctx.closePath();
 }
 
+let drawBricks = () => {
+  for(var c = 0; c < brickColumnCount; c++) {
+    for(var r = 0; r < brickRowCount; r++) {
+      var brickX = (c * (brickWidth+brickPadding)) + brickOffsetLeft;
+      var brickY = (r * (brickHeight+brickPadding)) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = '#0095DD';
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+};
+
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
 
@@ -57,6 +89,7 @@ const draw = () => {
     if(x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
     } else {  
+      // alert("Game Over");
       document.location.reload();
     } 
   }
