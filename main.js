62e32e486aadbc1799ee3c9607af43bf12cd0b52
praveen-me <praveenkrsaini0@gmail.com>
2018-10-08 23:17:09 +0530

    Add mouse control functionality

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ for(let c = 0; c < brickColumnCount; c++) {
 
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
+document.addEventListener('mousemove', mouseMoveHandler, false);
 
 function keyDownHandler(e) {
   if(e.keyCode === 39) {
@@ -43,6 +44,13 @@ function keyUpHandler(e) {
     rightPressed = false;
   } else if(e.keyCode === 37) {
     leftPressed = false;
+  }
+}
+
+function mouseMoveHandler(e) {
+  let relativeX = e.clientX - canvas.offsetLeft;
+  if(relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
   }
 }
 
